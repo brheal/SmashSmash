c1577893601342ff3036f4ba77310b75e2411dc0
Bryan Rhea <brheal@gmail.com>
2016-04-27 15:54:16 -0400

    initial commit

---
+++ GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  SmashSmash
+//
+//  Created by Bryan Rhea on 4/25/16.
+//  Copyright (c) 2016 Bryan Rhea. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+}
